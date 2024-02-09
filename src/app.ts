ee4c2eb9157ce8029ef97d595b7ce216b2d05300
seniorit <seniorit.dev@gmail.com>
2024-02-09 05:49:35 -0400

    Version ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
+import dotenv from 'dotenv'; 
+dotenv.config();
+
 import { MemoryDB, createBot, createFlow, createProvider } from "@bot-whatsapp/bot";
 import { BaileysProvider } from "@bot-whatsapp/provider-baileys";
-//
-import { normalMessage, mediaMessage } from './messageHandler.js';
-import { flowBienvenida } from './controlFlow.js';
+import { addKeyword } from '@bot-whatsapp/bot';
+import { mediaMessage, normalMessage } from './messageHandler.js';
+
+const flowBienvenida = addKeyword("Hola").addAnswer(
+  `Bienvenido al Sistema`
+);
+const flowServicios = addKeyword("Servicios").addAnswer(
+  ""
+);
+
 
 const main = async () => {
   const provider = createProvider(BaileysProvider);
-  provider.initHttpServer(10000);
+
+  provider.initHttpServer(3002);
+
   normalMessage(provider);
+
   mediaMessage(provider);
 
   createBot({
